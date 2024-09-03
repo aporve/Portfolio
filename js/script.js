12ae63e8ee899b45186479c2442903e10858fe3b
Aporve Bhatnagar <aporvebhat@gmail.com>
2024-09-03 23:30:51 +0530

    Commit function code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Typing Animation
 
 let typed = new Typed(".typing", {
-    strings: ["MERN Stack Developer", "FrontEnd Developer", "BackEnd Developer", "Software Engineer" ],
+    strings: [ "MERN Stack Developer", "FrontEnd Developer", "BackEnd Developer", "Software Engineer" ],
     typeSpeed:60,
     BackSpeed:60,
     loop: true
@@ -28,10 +28,7 @@ for(let i=0 ; i<totalNavList; i++) {
     // console.log(a)
     a.addEventListener("click", function() {
 
-        for(let i=0 ; i<totalSection; i++) {
-            allSection[i].classList.remove("back-section")
-        }
-
+        removeBackSection()
         // if(allSection[i] == allSection[j]) {
         //     allSection[i].classList.remove("back-section")
         // }
@@ -42,7 +39,7 @@ for(let i=0 ; i<totalNavList; i++) {
                 // console.log()
                 // let backSection = navList[j].querySelector("a")
                 // backSection.classList.add("back-section")
-                allSection[j].classList.add("back-section")
+                addBackSection(j);
             }
             navList[j].querySelector('a').classList.remove("active");
         }
@@ -56,14 +53,54 @@ for(let i=0 ; i<totalNavList; i++) {
 
 }
 
+function removeBackSection() {
+    for(let i=0 ; i<totalSection; i++) {
+        allSection[i].classList.remove("back-section")
+    }
+}
+
+function addBackSection(num) {
+    allSection[num].classList.add("back-section")
+}
+
 function showSection(element) {
     // console.log(element.getAttribute("href").split('#'));
     for(let i=0 ; i<totalSection; i++) {
         allSection[i].classList.remove("active")
     }
-    const target = element.getAttribute("href").split('#')[1]
+    const target = element.getAttribute("href").split('#')[1];
     document.querySelector('#'+target).classList.add("active")
 }
+
+function updateNav(element) {
+
+    for(let i=0 ; i<totalNavList; i++ ) {
+        navList[i].querySelector('a').classList.remove('active');
+        const target = element.getAttribute("href").split('#')[1];
+
+        if(target === navList[i].querySelector('a').getAttribute("href").split("#")[1]) {
+            navList[i].querySelector('a').classList.add('active');
+        }
+
+
+
+    }
+
+
+}
+
+
+document.querySelector('.hire-me').addEventListener('click', function(){
+    console.log('hire',this)
+
+    const sectionIndex = this.getAttribute('data-section-index')
+    console.log(sectionIndex)
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
+
 
 const navTogglerBtn = document.querySelector(".nav-toggler"),
       aside = document.querySelector(".aside");
@@ -84,6 +121,27 @@ function asideSectionToggleBtn() {
 
 
 
+// Form control 
+
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // const name = encodeURIComponent(document.getElementById('name').value);
+    // const email = encodeURIComponent(document.getElementById('email').value);
+    // const message = encodeURIComponent(document.getElementById('message').value);
+    // const subject = encodeURIComponent(document.getElementById('subject').value);
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const subject = document.getElementById('subject').value;
+
+    const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${message}`
+
+    window.open(`${mailtoLink}`, '_blank', 'width=800,height=600');
+    document.getElementById('emailForm').reset();
+});
 
 
 
